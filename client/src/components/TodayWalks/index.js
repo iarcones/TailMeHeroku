9cea4f46e411cc79c63b701c2e7cd7c2e3bf2e86
Isabel <iarcones@gmail.com>
2019-03-07 15:59:59 -0800

    timmer 5 minutes

--- client/src/components/TodayWalks/index.js
+++ client/src/components/TodayWalks/index.js
@@ -254,7 +254,7 @@ class TodayWalks extends Component {
 
                     navigator.geolocation.getCurrentPosition(success, error, options);
 
-                }, 60000);
+                }, 300000);
 
 
                 API.updateCheckInOut("in", walkId, coords.latitude, coords.longitude, dogData)
